cede557fea579e54912eff736332793c8e2e1f22
spoyodevelop <kjy9505@gmail.com>
2025-03-29 02:57:10 +0900

    Updates

--- assets/index-BeKSrm69.js
+++ assets/index-BeKSrm69.js
@@ -431,7 +431,7 @@ function Hero() {
 }
 function MovieItem({ id, src, title, rate, onload }) {
   const $li = createElement("li", { id });
-  let url = `https://image.tmdb.org/t/p/w500/${src}`;
+  let url = `https://image.tmdb.org/t/p/w500${src}`;
   if (!src) url = "images/fallback.png";
   $li.innerHTML = `
       <div class="skeleton-thumbnail thumbnail"></div>
